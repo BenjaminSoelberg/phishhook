e166f3b2e1c278c5764aa9b5d26a4e51cc11202e
Benjamin Sølberg <benjamin.soelberg@gmail.com>
2023-03-13 19:09:11 +0100

    Added trigger_words and score_words to the brand. Updated example brands.json Code cleanup

--- testdata.py
+++
@@ -1,9 +0,0 @@
-from brand import Brand
-# TODO: Fix me!
-if __name__ == '__main__':
-    github = Brand(name='GitHub', brand='github', tlds=['com', 'co.uk'], sub_domains=['www', '*.services'])
-    apple = Brand(name='Apple', brand='apple', tlds=['com', 'dk'], sub_domains=['www', 'icloud'])
-    json = Brand.Schema().dumps([github, apple], many=True, indent='\t');
-
-    with open(file='rules.json', mode='w+') as f:
-        f.write(json)
