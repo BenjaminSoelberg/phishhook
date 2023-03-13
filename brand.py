0dc2f3f557a5ab3db8d8e2c2a3ec2dc2d00398b4
Benjamin Sølberg <benjamin.soelberg@gmail.com>
2023-03-13 19:09:11 +0100

    Added trigger_words and score_words to the brand. Updated example brands.json Code cleanup

--- brand.py
+++ brand.py
@@ -7,8 +7,9 @@ from marshmallow_dataclass import add_schema
 @dataclass(eq=True, frozen=True)
 class Brand:
     brand: str
-    names: list[str]
     known_domains: list[str]
+    trigger_words: list[str]
+    score_words: list[str]
     enabled: bool = True
 
     class Meta:
